8b8ce5dda0f0caa0b7c5e53a5d7672818bd0cfe8
hikiray <satalomaksimka12345678910@gmail.com>
2026-02-01 00:08:35 +0700

    ll

--- include/player.hpp
+++ include/player.hpp
@@ -1,8 +1,20 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+<<<<<<< HEAD
 class Player{
     
+=======
+#include "object.hhp"
+#include "string"
+
+class Player : public Object {
+private:
+    unsigned int health;
+    std::string name;
+public:
+    unsigned int attack() = 0;
+>>>>>>> 0974d30 (ll)
 };
 
 #endif
